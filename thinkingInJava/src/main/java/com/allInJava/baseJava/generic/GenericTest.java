bb5a44174a9189283fc4a9f67993ecf4998846e2
yuhang <649411629@qq.com>
2019-06-03 17:35:45 +0800

    添加泛型

---
+++ thinkingInJava/src/main/java/com/allInJava/baseJava/generic/GenericTest.java
@@ -0,0 +1,16 @@
+package com.allInJava.baseJava.generic;
+
+/**
+ * @Description:
+ * @Author: yuhang
+ * @Date: 2019/6/3 15:40
+ */
+public class GenericTest {
+    public static void main(String[] args) {
+        Box<String> stringBox = new Box<>("hello");
+        Box<Integer> integerBox = Box.success(35);
+//        integerBox.getData()
+        System.out.println(stringBox.getData());
+    }
+
+}
